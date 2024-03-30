68071b1ae0f7682c7bbceb6d629c5294f3209193
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2024-03-30 06:59:32 -0400

    manifest added

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,33 @@
-const { response } = require("express");
-const usersGet = (req, res = response) => {
+const { response, request } = require("express");
+
+// Endpoint GET
+const usersGet = (req = request, res = response) => {
+  const { q, name = 'No Name', apiKey } = req.query;
   res.json({
-    ok: true,
-    msg: "get - API - controller",
+    q,
+    name,
+    apiKey,
   });
 };
 
+// Endpoint PUT
 const usersPut = (req, res = response) => {
+  const id = req.params.id;
   res.status(400).json({
-    ok: true,
     msg: "put - API - controller",
+    id,
   });
 };
 
+// Endpoint POST
 const usersPost = (req, res) => {
   res.status(201).json({
     ok: true,
     msg: "post - API - controller",
   });
 };
+
+// Endpoint DELETE
 const usersDelete = (req, res) => {
   res.json({
     ok: true,
